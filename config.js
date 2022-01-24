379246617b37e85cb5bf8697180b17c7cba1ffd0
Kais Rouini <kais.rouini@gmail.com>
2022-01-24 18:31:35 +0100

    redeploy contracts

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = "0x76a0c79539611B1E050E6Db3571b3f13411B8Ad4"
-export const nftaddress = "0x5B9861347C74DCb1C438451c238197695151Fc8b"
+export const nftmarketaddress = "0x23a5Fc9D1ae2ff92B48A2c5Cedac5678CdeE5D99"
+export const nftaddress = "0xb28331C99F69150c8e2fFC5E52fACD86b58A3148"
