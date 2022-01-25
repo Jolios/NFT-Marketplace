89fa5fbbd2da90d621bc4eec8a92ed284c1af349
Kais Rouini <kais.rouini@gmail.com>
2022-01-25 12:08:52 +0100

    resell works

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = "0x23a5Fc9D1ae2ff92B48A2c5Cedac5678CdeE5D99"
-export const nftaddress = "0xb28331C99F69150c8e2fFC5E52fACD86b58A3148"
+export const nftmarketaddress = "0xDc332604Fa1a0fc758056cbf1647D14204Bf42B5"
+export const nftaddress = "0x596Fb974feD454e88f3a5Fa37D14a332a9d15526"
